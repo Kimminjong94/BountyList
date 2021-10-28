4c7fdbce1a8156a2e238fe0c1d35762c2d02f185
minjongkim <rlaalswhd200@naver.com>
2021-10-29 06:03:41 +0900

    Initial Commit

---
+++ BountyList/BountyViewController.swift
@@ -0,0 +1,70 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by 김민종 on 2021/10/03.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro" ]
+    let bountyList = [300000, 20, 44000000, 500000, 200000, 3420000, 10000000, 200000000]
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            let vc = segue.destination as? DetailViewController
+            if let index = sender as? Int {
+                vc?.name = nameList[index]
+                vc?.bounty = bountyList[index]
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//
+//        return cell
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+            
+            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+            cell.imgView.image = img
+            cell.nameLabel.text = nameList[indexPath.row]
+            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+            return cell
+            
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    }
+    
+}
+
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+
+}
